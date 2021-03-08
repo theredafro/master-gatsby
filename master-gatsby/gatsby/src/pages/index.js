66a03434ba118b8841ac95e0229e0cbaad22878d
Jimmy Gitonga <jimmy@afroshok.com>
2021-03-09 00:01:33 +0300

    REFACTOR: Update all the dependencies for Gatsby and Sanity.

---
+++ master-gatsby/gatsby/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="HomePage">
+      <h2> Hey You!</h2>
+      <p>Hello world!</p>
+    </div>
+  );
+}
